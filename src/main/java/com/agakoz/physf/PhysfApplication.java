3acaf96fd9074402a3c6542c7d3e68fe628f7bba
agakoz <agataakoziol@gmail.com>
2020-05-19 14:29:18 +0200

    initial commit

---
+++ src/main/java/com/agakoz/physf/PhysfApplication.java
@@ -0,0 +1,13 @@
+package com.agakoz.physf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agakoz/physf/PhysfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agakoz.physf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
