6f5e03920487801a3bbf43cdf4ca052547a92f1b
agakoz <agataakoziol@gmail.com>
2020-06-02 17:23:42 +0200

    swagger

--- src/main/java/com/agakoz/physf/PhysfApplication.java
+++ src/main/java/com/agakoz/physf/PhysfApplication.java
@@ -2,7 +2,10 @@ package com.agakoz.physf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class PhysfApplication {
 
